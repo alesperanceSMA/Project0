589257e76de24523825479357735946253e0b7b5
alesperanceSMA <alex.lesperance@smacatholic.org>
2021-01-13 23:29:54 -0600

    Adding basic java project

---
+++ Project0/src/hello/Hello.java
@@ -0,0 +1,15 @@
+package hello;
+
+public class Hello
+{
+    public static void main(String[] args)
+    {
+        //TODO: put in your name
+        sayHello("");
+    }
+    
+    private static void sayHello(String name)
+    {
+        System.out.println("Hello, " + name);
+    }
+}
